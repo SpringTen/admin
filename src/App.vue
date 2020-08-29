b2bd9d982fcc2e9aee699c1dc3f5516a9ae1be74
SpringTen <737612552@qq.com>
2020-08-29 14:50:08 +0800

    结构搭建

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
     axios,
   },
   data() {
     return {
-      age: 20
     }
   },
   mounted() {
-    let url = "https://www.easy-mock.com/mock/5f49c15e9279d93141e87099/example/restful/:id/list";
-    axios.get(url).then(() => {
-
-    })
   }
 }
 </script>
